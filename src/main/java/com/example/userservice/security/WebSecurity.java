8c99bad411699c236c542099172ff5e67e91ab6d
Doobu-L <seop00513@gmail.com>
2022-09-17 23:29:43 +0900

    Security Login 구현 1#

--- src/main/java/com/example/userservice/security/WebSecurity.java
+++ src/main/java/com/example/userservice/security/WebSecurity.java
@@ -1,9 +1,11 @@
 package com.example.userservice.security;
 
-import com.netflix.discovery.converters.Auto;
+import com.example.userservice.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -13,12 +15,44 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 @EnableWebSecurity
 public class WebSecurity extends WebSecurityConfigurerAdapter {
 
+    private UserService userService;
+    private BCryptPasswordEncoder passwordEncoder;
+    private Environment env;
+
+    @Autowired
+    public WebSecurity(Environment env,UserService userService, BCryptPasswordEncoder passwordEncoder) {
+        this.env = env;
+        this.userService = userService;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public WebSecurity(boolean disableDefaults, UserService userService, BCryptPasswordEncoder passwordEncoder) {
+        super(disableDefaults);
+        this.userService = userService;
+        this.passwordEncoder = passwordEncoder;
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
-        http.authorizeHttpRequests().antMatchers("/users/**").permitAll();
+        //http.authorizeHttpRequests().antMatchers("/users/**").permitAll();
+        http.authorizeRequests().antMatchers("/**")
+                .hasIpAddress("192.168.0.19")
+                .and()
+                .addFilter(getAuthenticationFilter());
 
         http.headers().frameOptions().disable();
+    }
+
+    private AuthenticationFilter getAuthenticationFilter() throws Exception {
+        AuthenticationFilter authenticationFilter = new AuthenticationFilter();
+        authenticationFilter.setAuthenticationManager(authenticationManager());
+        return authenticationFilter;
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userService).passwordEncoder(passwordEncoder());
     }
 
     @Bean
